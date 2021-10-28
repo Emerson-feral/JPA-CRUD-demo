ec36a1339d6d82566d5d09a3a0cadf4a9e3bb546
Emerson Fernandes <emerson_feral@hotmail.com>
2021-10-28 12:28:18 +0200

    Product entity

---
+++ crud/src/main/java/com/jpacrud/crud/entity/Product.java
@@ -0,0 +1,57 @@
+package com.jpacrud.crud.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Integer quantity;
+    private double price;
+
+    public Product(){
+
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+}
